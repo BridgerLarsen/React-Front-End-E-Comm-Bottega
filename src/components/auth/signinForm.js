6ea0885490a28f3d37eb51e2f9f9a252816b23c1
Bridger Larsen <bridgerjimmy@gmail.com>
2021-04-12 20:52:51 -0600

    Styled formFields components for signin page/component

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -30,6 +30,8 @@ class SignInForm extends Component {
                     component={FormInputs} 
                 />
 
+                <div className="sign-in-form__line"></div>
+
                 <Field
                     className="sign-in-form__login"
                     onClick={() => { console.log('submiting') }}
