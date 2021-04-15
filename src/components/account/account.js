7a853bfe33246e5bfa3dea59b4d565911ca2d799
Bridger Larsen <bridgerjimmy@gmail.com>
2021-04-14 18:13:43 -0600

    Finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -12,6 +12,19 @@ class Account extends Component {
     }
 
     componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
+
         const navbarLinks = [
             {
                 _id: 0,
@@ -27,8 +40,13 @@ class Account extends Component {
             }
         ]
 
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
+    }
+
+    componentWillUnmount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
     }
 
     renderContent() {
